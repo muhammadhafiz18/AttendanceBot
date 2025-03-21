0c99b5a1861518710c036be7146f57a7b3004dc6
muhammadhafiz <khodjayev.muhammad@gmail.com>
2025-03-22 01:06:45 +0500

    Add project files.

---
+++ AttendanceBot/Attendance.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AttendanceBot;
+
+public class Attendance(ILogger<Attendance> logger)
+{
+    private readonly ILogger<Attendance> logger = logger;
+
+    [Function("Attendance")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
